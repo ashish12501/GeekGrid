dd1e020e9694ffefaccdfe5c8595b6baf9315e0b
ashish12501 <ay079396@gmail.com>
2023-10-18 15:24:24 +0530

    added Footer

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Footer } from "../../components/footer"
 export function Home() {
   return (
     <div>
-      <h1>ashish is homo</h1>
+      <h1>Imported footer here !</h1>
+      <Footer />
     </div>
   )
 }
-
