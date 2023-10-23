7372ce5698351d84bf530e2773e72e105494fae9
ashish12501 <ay079396@gmail.com>
2023-10-23 21:22:39 +0530

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './navbar.css'
+export function Navbar() {
+    return (
+        <div>
+            <h1>This is my Navbar</h1>
+        </div>
+    )
+}
+
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 // import { Footer } from "../../components/footer"
+import { Navbar } from "../../components/navbar"
+
 export function Home() {
   return (
     <div>
       {/* <h1>Imported footer here !</h1> */}
       {/* <Footer /> */}
+      <Navbar />
       <h1>Navigate to your respective Routes to continue you work !</h1>
       <h2 style={{ color: "red" }} >Avoid Pushing to the main branch </h2>
       <h4>Aman/Aparna: <span style={{ color: "blue" }}>'/articles'</span></h4>
