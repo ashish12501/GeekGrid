c8afef50e3b1bcd56e1f8f3fff3c54c49fd8f528
ashish12501 <ay079396@gmail.com>
2023-10-25 21:20:31 +0530

    added navagation to home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,11 +8,11 @@ export function Navbar() {
                 <logo><h2 className='logo-txt'>GeekGrid</h2></logo>
                 <links>
                     <ul>
+                        <li><Link to="/" className='links'>Home</Link></li>
                         <li><Link to="/learn" className='links'>Learn</Link></li>
                         <li><Link to="/articles" className='links'>Articles</Link></li>
                         <li><Link to="/gethired" className='links'>Get Hired</Link></li>
                         <li><Link to="/about" className='links'>About</Link></li>
-                        <li><Link to="/contact" className='links'>Contact</Link></li>
                     </ul>
                 </links>
             </div>
