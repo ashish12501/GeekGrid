229d88a14964c57791ace9af23f2a8202c49355f
ashish12501 <ay079396@gmail.com>
2023-10-28 15:06:14 +0530

    started working on  video streaming page

--- src/pages/CoursePlayer/coursePlayer.js
+++ src/pages/CoursePlayer/coursePlayer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { Streamer } from '../../components/streamer'
 export function CoursePlayer() {
     return (
         <div>
-
+            <Streamer />
         </div>
     )
 }
