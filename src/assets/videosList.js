22ed18e58e001c0257e2b11b3fff49367f0def43
ashish12501 <ay079396@gmail.com>
2023-10-31 07:09:38 +0530

    added suggested videos and about instructor

--- src/assets/videosList.js
+++ src/assets/videosList.js
@@ -4,13 +4,16 @@ export const VideoList = [
         id: '1',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava"
     },
     {
         id: '2',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava"
+
     },
 
 
@@ -21,57 +24,77 @@ export const VideoList = [
         id: '3',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava"
+
     },
     {
         id: '4',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava"
+
     }, {
         id: '5',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava"
+
     },
     {
         id: '6',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava",
+
     }, {
         id: '7',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava",
+
     },
     {
         id: '8',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava",
+
     }, {
         id: '9',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava",
+
     },
     {
         id: '10',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava",
+
     }, {
         id: '11',
         name: 'Basics of Command Line (CMD)',
         image: require('../assets/images/CMD.jpg'),
-        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc"
+        embedLink: "https://www.youtube.com/embed/QZ71hx19qcg?si=4dmdcRYWxdo1HSIc",
+        instructorname: "Aman Shrivastava",
+
     },
     {
         id: '12',
         name: 'PM Speech Analysis Project overview',
         image: require('../assets/images/Namo.jpg'),
-        embedLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        embedLink: 'https://www.youtube.com/embed/WIOVqO8xqts?si=ZOfTn4kKGUkVuU86',
+        instructorname: "Aman Shrivastava",
+
     },
     // Add more video objects as needed
 ];
