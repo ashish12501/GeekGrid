9ea453821c86bf243e266c0b2ff32e7ad90cc547
ashish12501 <ay079396@gmail.com>
2023-11-04 19:09:21 +0530

    edited switch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,7 +22,7 @@ export function Navbar() {
                 </links>
             </div>
             <div className='navbar-right'>
-                {theme === "light" ? <p className='mode-name'>Light mode</p> : <p className='mode-name'>Dark mode</p>}
+                {/* {theme === "light" ? <p className='mode-name'>Light mode</p> : <p className='mode-name'>Dark mode</p>} */}
                 <ReactSwitch className='toggle-button' onChange={toggleTheme} checked={theme === "light"} onColor="#02A960" />
                 <button>Login / SignUp</button>
             </div>
