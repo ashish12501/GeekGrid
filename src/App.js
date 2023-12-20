f69f859d344a359eec899fa484ae39fce76d2fa5
ashish12501 <ay079396@gmail.com>
2023-12-20 22:49:44 +0530

    structuring jobs

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/home'
-import { Contact } from './pages/Contact/contact'
-import { Signin } from './pages/Signin/signin'
-import { Signup } from './pages/Signup/signup'
-import { AddData } from './pages/Admin/addData'
-import { Articles } from './pages/Articles/articles'
-import { Navbar } from './components/navbar'
-import { Footer } from './components/footer'
-import { VideoListPage } from './pages/VideoCourses/VideoListPage'
-import { VideoPlayerPage } from './pages/VideoCourses/streamer'
-import './variables.css'
-import { useEffect, useState } from 'react';
-import { createContext } from 'react'
-import { auth } from './config/firebase-config'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/home";
+import { Contact } from "./pages/Contact/contact";
+import { Signin } from "./pages/Signin/signin";
+import { Signup } from "./pages/Signup/signup";
+import { AddData } from "./pages/Admin/addData";
+import { Articles } from "./pages/Articles/articles";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
+import { VideoListPage } from "./pages/VideoCourses/VideoListPage";
+import { VideoPlayerPage } from "./pages/VideoCourses/streamer";
+import { JobList } from "./pages/GetHired/getHired";
+import "./variables.css";
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import { auth } from "./config/firebase-config";
 
-export const themeContext = createContext(null)
+export const themeContext = createContext(null);
 
 function App() {
-
-
-  const [userData, setUserData] = useState([])
-
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setUserData(user)
-    })
-  })
+      setUserData(user);
+    });
+  });
 
-
-  const [theme, setTheme] = useState("dark")
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark")
-  }
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
-    <themeContext.Provider value={{ theme, toggleTheme, userData }} >
-      <div className="App" id={theme} >
+    <themeContext.Provider value={{ theme, toggleTheme, userData }}>
+      <div className="App" id={theme}>
         <Router>
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/contacts' element={<Contact />} />
-            <Route path='/signin' element={<Signin />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/articles' element={<Articles />} />
-            <Route path='/admin' element={<AddData />} />
-            <Route path='/streamer/:id' element={<VideoPlayerPage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/contacts" element={<Contact />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/admin" element={<AddData />} />
+            <Route path="/streamer/:id" element={<VideoPlayerPage />} />
             <Route path="/learn" element={<VideoListPage />} />
+            <Route path="/gethired" element={<JobList />} />
           </Routes>
           <Footer />
         </Router>
-      </div >
+      </div>
     </themeContext.Provider>
   );
 }
