0df57abe0530c1124a65da63b28ed4ecd0e4e1e4
ashishyadav63 <ashish.yadav@revspire.net>
2024-01-03 15:51:51 +0530

    test bwtn account switches

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   });
 
   const [theme, setTheme] = useState("light");
-
+  //  Theme Toggling goes here !!
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
